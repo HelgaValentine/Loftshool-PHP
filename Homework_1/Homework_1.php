584a7f6e542a4d3d4a8eb6afd8eae8ee9005e18e
Kozhushko Olha <helga.valentine742@gmail.com>
2018-06-10 20:40:57 +0300

    Update Homework №1

--- Homework_1/Homework_1.php
+++ Homework_1/Homework_1.php
@@ -9,7 +9,6 @@ echo "Мне $age лет" . '<br>';
 echo '"!|\/\'"\\' . '<br><br><br>';
 
 
-
 echo '<b>' . 'Task #2' . '</b><br><br>';
 
 const DRAWINGS = 80;
@@ -17,36 +16,31 @@ const FELTPENS = 23;
 const PENSILS = 40;
 const PAINTS = DRAWINGS - FELTPENS - PENSILS;
 echo "Условие задачи: на школьной выставке " . DRAWINGS . " рисунков, " .
-FELTPENS . " из них выполнены фломастерами, " . PENSILS . " карандашами, а остальные — красками. 
+    FELTPENS . " из них выполнены фломастерами, " . PENSILS . " карандашами, а остальные — красками. 
 Сколько рисунков выполнены красками?" . '<br>';
 echo "Ответ: красками нарисованы " . PAINTS . " рисунков.";
 echo '<br><br><br>';
 
 
-
 echo '<b>' . 'Task #3' . '</b><br><br>';
 
-$age = rand (-20, 100);
+$age = rand(-20, 100);
 echo "Ваш возраст: $age." . '<br>';
 if (18 <= $age && $age <= 65) {
     echo 'Вам еще работать и работать.';
-}
-elseif (1 <= $age && $age <= 17) {
+} elseif (1 <= $age && $age <= 17) {
     echo 'Вам   ещё   рано   работать.';
-}
-elseif ($age > 65) {
+} elseif ($age > 65) {
     echo 'Вам пора на пенсию.';
-}
-else {
+} else {
     echo 'Неизвестный возраст.';
 }
 echo '<br><br><br>';
 
 
-
 echo '<b>' . 'Task #4' . '</b><br><br>';
 
-$day = rand (1, 10);
+$day = rand(1, 10);
 echo "День № $day" . '<br>';
 switch ($day) {
     case 1:
@@ -66,7 +60,6 @@ switch ($day) {
 echo '<br><br><br>';
 
 
-
 echo '<b>' . 'Task #5' . '</b><br><br>';
 
 $bmw = ['model' => 'X5',
@@ -81,14 +74,13 @@ $opel = ['model' => 'Vectra',
     'speed' => 100,
     'doors' => 5,
     'year' => 2003];
-$cars = ['bmv'=>$bmw,
-    'toyota'=>$toyota,
-    'opel'=>$opel];
+$cars = ['bmv' => $bmw,
+    'toyota' => $toyota,
+    'opel' => $opel];
 foreach ($cars as $key => $value) {
     echo "Car $key<br>";
     echo "{$value["model"]} {$value["speed"]} {$value["doors"]} {$value["year"]}<br><br><br>";
 }
-
 
 
 echo '<b>' . 'Task #6' . '</b><br><br>';
@@ -104,8 +96,7 @@ for ($i = 1; $i <= $rows; $i++) {
         $number = $i * $j;
         if ($rowCheck !== 0 && $columnCheck !== 0) {
             $number = "<b> [$number] </b>";
-        }
-        elseif ($rowCheck === 0 && $columnCheck === 0) {
+        } elseif ($rowCheck === 0 && $columnCheck === 0) {
             $number = "<b> ($number) </b>";
         }
         echo "<td style='width: 25px; text-align: center'> $number </td>";
